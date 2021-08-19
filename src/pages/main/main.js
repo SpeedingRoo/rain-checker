21eda3cab9d25f0e69bb9d755cf4304063e4ad3b
SpeedingRoo <speedingroo@gmail.com>
2021-08-20 02:00:28 +1000

    successfully connected the API and get the weather data using axios

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,86 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { SBody, SForm, SInput, SButton } from '../../components/styles';
-import axios from 'axios';
-import Card from '../../components/card';
+import React, { useState } from "react";
+import { SBody, SForm, SInput, SButton } from "../../components/styles";
+import axios from "axios";
+import Card from "../../components/card";
 
 const MainPage = () => {
+  const [city, setCity] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [weatherData, setWeatherData] = useState("");
+  const APIkey = "f620a018d0f4d826614d158f97a3f828";
+  const APIcallUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
 
-    const [city,setCity] = useState('');
-    const [counter,setCounter] = useState(0)
-    const [weatherData,setWeatherData] = useState('');
-    const APIkey = 'f620a018d0f4d826614d158f97a3f828';
-    const APIcallUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`
+  function getInput(e) {
+    setCity(e.target.value);
+  }
 
-    function getInput(e) {
-        setCity(e.target.value);
-    }
+  function clearField(e) {
+    e.target.value = city;
+  }
 
-    function clearField(e){
-        e.target.value=city;
-    }
+  async function myOnClick() {
+    await axios({
+            method: 'get',
+            url: APIcallUrl,
+        })
+        .then( res => {
+                    setWeatherData(res.data.main.temp);
+                    console.warn(res);
+        })
+        .catch( err => {
+            console.log(err);
+        });
 
-    function myOnClick() {
+  }
 
-            axios.get(APIcallUrl).then(
-                res=>{
-                    setWeatherData(res.json());
-                    console.log(`The weather in Rio is : ${weatherData}`);
-                    }
-                )
-            
-            window.alert(city)
-    }
+  function minus() {
+    setCounter(counter - 1);
+  }
 
-    function minus() {
-        setCounter(counter-1);
-    }        
+  function plus() {
+    setCounter(counter + 1);
+  }
 
-    function plus() {
-        setCounter(counter+1);
-    }
+  function reset() {
+    setCounter(0);
+  }
 
-    function reset() {
-        setCounter(0);
-    }
-
-    return (
-        <SBody className='main-page'>
-            <section>
-                <SForm>
-                    <SInput type='text' 
-                            defaultValue='start typying here'
-                            onChange={getInput}
-                            onFocus={clearField}
-                    />
-                </SForm>
-                <SButton type='button' onClick={myOnClick} >Check</SButton>
-            </section>
-            <section>
-                <span>{city}</span>
-            </section>
-            <section>
-                <SButton onClick={minus}>
-                    MINUS - 
-                </SButton>
-                <SButton onClick={reset}>
-                    / refresh /
-                </SButton>
-                <SButton onClick={plus}>
-                    PLUS + 
-                </SButton>
-            </section>
-            <section>
-                <Card city='Beijing' temp='-10'/>
-                <Card city='London' temp='5' />
-            </section>
-            <section>
-                <p>
-                    clicked <span>{counter}</span> times
-                </p>
-                <p>the weather data is {weatherData}</p>
-            </section>
-        </SBody>
-    );
+  return (
+    <SBody className="main-page">
+      <section>
+        <SForm>
+          <SInput
+            type="text"
+            defaultValue="start typying here"
+            onChange={getInput}
+            onFocus={clearField}
+          />
+        </SForm>
+        <SButton type="button" onClick={myOnClick}>
+          Check
+        </SButton>
+      </section>
+      <section>
+        <span>{city}</span>
+      </section>
+      <section>
+        <SButton onClick={minus}>MINUS -</SButton>
+        <SButton onClick={reset}>/ refresh /</SButton>
+        <SButton onClick={plus}>PLUS +</SButton>
+      </section>
+      <section>
+        <Card city="Beijing" temp="-10" />
+        <Card city="London" temp="5" />
+      </section>
+      <section>
+        <p>
+          clicked <span>{counter}</span> times
+        </p>
+        <p>the weather data is { weatherData}</p>
+      </section>
+    </SBody>
+  );
 };
 
 export default MainPage;
